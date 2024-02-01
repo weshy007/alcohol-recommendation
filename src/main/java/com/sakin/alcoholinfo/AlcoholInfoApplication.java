bf578fc5171b0bec539d0a37d2f2e39f9e6b2c14
Weshy Joe <josephwaweru96@gmail.com>
2024-02-01 10:01:06 +0300

    initial commit

---
+++ src/main/java/com/sakin/alcoholinfo/AlcoholInfoApplication.java
@@ -0,0 +1,13 @@
+package com.sakin.alcoholinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcoholInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sakin/alcoholinfo/AlcoholInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakin.alcoholinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
